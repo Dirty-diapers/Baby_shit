c51345368bd77499392d259b006fefeb16f8ab55
Dorimanx <yuri@bynet.co.il>
2015-09-21 01:26:35 +0300

    CPU HOTPLUGS: convert all to use state notifier suspend.
    also converted all to use more simple work queue and merged updates
    to Alucard HotPlug from DEV.
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/intelli_hotplug.c
    	arch/arm/mach-msm/msm_hotplug.c
    	arch/arm/mach-msm/msm_rq_stats.c

--- arch/arm/mach-msm/msm_rq_stats.c
+++ arch/arm/mach-msm/msm_rq_stats.c
@@ -273,10 +273,6 @@ static ssize_t show_hotplug_enable(struct kobject *kobj,
 
 static struct kobj_attribute hotplug_disabled_attr = __ATTR_RO(hotplug_disable);
 
-static struct kobj_attribute hotplug_enabled_attr =
-	__ATTR(hotplug_enable, S_IWUSR | S_IRUSR, show_hotplug_enable,
-	       store_hotplug_enable);
-
 #ifdef CONFIG_BRICKED_HOTPLUG
 unsigned int get_rq_info(void)
 {
@@ -294,7 +290,6 @@ unsigned int get_rq_info(void)
 }
 EXPORT_SYMBOL(get_rq_info);
 #endif
-
 static void def_work_fn(struct work_struct *work)
 {
 	int64_t diff;
